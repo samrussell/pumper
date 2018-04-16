ada303800cca44b5ef4531785c10865699317dfe
Sam Russell <sam.h.russell@gmail.com>
2018-04-16 21:09:06 +1200

    Moved huffman table building into its own class

---
+++ lib/build_huffman_tables.rb
@@ -0,0 +1,50 @@
+require "./lib/huffman_table.rb"
+require "./lib/code_length_decoder.rb"
+
+class BuildHuffmanTables
+  CODE_LENGTHS_ARRAY = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
+  MINIMUM_LITERAL_LENGTH_CODES = 257
+  MINIMUM_DISTANCE_CODES = 1
+  MINIMUM_CODE_LENGTH_CODES = 4
+
+  def initialize(bitstream)
+    @bitstream = bitstream
+  end
+
+  def distance_table
+    build_huffman_tables unless @distance_table
+
+    @distance_table
+  end
+
+  def literal_length_table
+    build_huffman_tables unless @literal_length_table
+
+    @literal_length_table
+  end
+
+  private
+
+  def build_huffman_tables
+    num_literal_length_codes = MINIMUM_LITERAL_LENGTH_CODES + @bitstream.read_number(5)
+    num_distance_codes = MINIMUM_DISTANCE_CODES + @bitstream.read_number(5)
+    num_code_length_codes = MINIMUM_CODE_LENGTH_CODES + @bitstream.read_number(4)
+    unsorted_code_length_codes = num_code_length_codes.times.map { @bitstream.read_number(3) }
+    code_lengths_dictionary = CODE_LENGTHS_ARRAY.zip(unsorted_code_length_codes).each.with_object({}) do |(code, length), hash|
+      if length && length > 0
+        hash[code] = length
+      else
+        hash[code] = nil
+      end
+    end
+    code_lengths = code_lengths_dictionary.sort.map {|x| x[1]}
+
+    code_length_table = HuffmanTable.new(code_lengths)
+
+    literal_length_codes = CodeLengthDecoder.new(@bitstream, code_length_table).decode(num_literal_length_codes)
+    @literal_length_table = HuffmanTable.new(literal_length_codes)
+
+    distance_codes = CodeLengthDecoder.new(@bitstream, code_length_table).decode(num_distance_codes)
+    @distance_table = HuffmanTable.new(distance_codes)
+  end
+end
--- lib/unhuffer.rb
+++ lib/unhuffer.rb
@@ -1,17 +1,12 @@
 require "./lib/bitstream.rb"
-require "./lib/huffman_table.rb"
 require "./lib/length_decoder.rb"
 require "./lib/distance_decoder.rb"
-require "./lib/code_length_decoder.rb"
 require "./lib/lz77_decoder.rb"
+require "./lib/build_huffman_tables.rb"
 
 class Unhuffer
   FIXED_HUFFMAN_CODES = 1
   DYNAMIC_HUFFMAN_CODES = 2
-  CODE_LENGTHS_ARRAY = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
-  MINIMUM_LITERAL_LENGTH_CODES = 257
-  MINIMUM_DISTANCE_CODES = 1
-  MINIMUM_CODE_LENGTH_CODES = 4
 
   def initialize(block_file)
     @block_file = block_file
@@ -22,7 +17,10 @@ class Unhuffer
 
     read_header
 
-    build_huffman_tables
+    huffman_table_builder = BuildHuffmanTables.new(@bitstream)
+
+    @literal_length_table = huffman_table_builder.literal_length_table
+    @distance_table = huffman_table_builder.distance_table
 
     lz77_encoded_data = decode_huffman
 
@@ -64,28 +62,5 @@ class Unhuffer
     final_flag = @bitstream.read(1)
     encoding_type = @bitstream.read_number(2)
     raise "Can only handle dynamic codes" unless encoding_type == DYNAMIC_HUFFMAN_CODES
-  end
-
-  def build_huffman_tables
-    num_literal_length_codes = MINIMUM_LITERAL_LENGTH_CODES + @bitstream.read_number(5)
-    num_distance_codes = MINIMUM_DISTANCE_CODES + @bitstream.read_number(5)
-    num_code_length_codes = MINIMUM_CODE_LENGTH_CODES + @bitstream.read_number(4)
-    unsorted_code_length_codes = num_code_length_codes.times.map { @bitstream.read_number(3) }
-    code_lengths_dictionary = CODE_LENGTHS_ARRAY.zip(unsorted_code_length_codes).each.with_object({}) do |(code, length), hash|
-      if length && length > 0
-        hash[code] = length
-      else
-        hash[code] = nil
-      end
-    end
-    code_lengths = code_lengths_dictionary.sort.map {|x| x[1]}
-
-    code_length_table = HuffmanTable.new(code_lengths)
-
-    literal_length_codes = CodeLengthDecoder.new(@bitstream, code_length_table).decode(num_literal_length_codes)
-    @literal_length_table = HuffmanTable.new(literal_length_codes)
-
-    distance_codes = CodeLengthDecoder.new(@bitstream, code_length_table).decode(num_distance_codes)
-    @distance_table = HuffmanTable.new(distance_codes)
   end
 end
